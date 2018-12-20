4092328173b965840a0c972de078363a583a950a
ming543 <ming543@gmail.com>
2018-12-20 16:37:24 +0800

    update

--- x.sh
+++ x.sh
@@ -1,2 +1,2 @@
-cd /run/initramfs/memory/data/script/
-xterm -e /run/initramfs/memory/data/script/start.sh
+cd /run/initramfs/memory/data/HW_test_script/
+xterm -e /run/initramfs/memory/data/HW_test_script/start.sh
