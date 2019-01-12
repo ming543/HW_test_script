c38474338b8c570170eb0cbfa3b625e18395e78c
ming <45988196+ming543@users.noreply.github.com>
2019-01-12 23:45:59 +0800

    Update SITEID.sh

--- SITEID.sh
+++ SITEID.sh
@@ -13,11 +13,14 @@
 #sn check
 . ./lib/siteid_sn_test.lib
 
-#Temp check
+#Temp check set low and high range ex. 18 26
+. ./lib/siteid_temp_test.lib 18 26
 
 #door check
+. ./lib/siteid_door_test.lib
 
 #memory check
+. ./lib/siteid_memory_test.lib
   
 #Show ALL TEST PASS to log
 . ./lib/pass_green.lib
