36950d6f1ef797fdda87322ac4c1afff7e9f55db
ming543 <ming543@gmail.com>
2019-01-17 08:58:50 +0800

    0116

--- master_setup/server_start.sh
+++ master_setup/server_start.sh
@@ -4,7 +4,8 @@
 
 TOOL=/run/initramfs/memory/data/HW_test_script/tool
 SERVER_RUN=/run/initramfs/memory/data/HW_test_script/master_setup
-HOST=192.168.1.254
+#HOST=192.168.1.254
+HOST=10.0.0.6
 LAN1=enp3s0
 
 #set timezone to Taipei
