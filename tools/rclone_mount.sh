a4589b50e598bd80538f86b3593e47b3885f2fc7
ming543 <ming543@gmail.com>
2020-11-30 14:16:32 +0800

    os_image

--- tools/rclone_mount.sh
+++ tools/rclone_mount.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-rclone mount onedrive:General /home/production/onedrive_mount &
+#rclone mount onedrive:General /home/production/onedrive_mount &
+rclone mount os:OS_IMAGE /home/production/onedrive_mount &
 
