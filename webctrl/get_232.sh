ec550a7ef673d77a07e61153e59f6491f4e3e95c
ming <ming543@gmail.com>
2020-12-22 15:35:08 +0800

    os_update

--- webctrl/get_232.sh
+++ webctrl/get_232.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DUT=192.168.3.170
+DUT=192.168.3.2
 
 curl -X GET http://$DUT/__/config/port/RS/1 | jq
 #curl -X GET http://$DUT/__/bundle | jq
--- webctrl/post_232.sh
+++ webctrl/post_232.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DUT=192.168.3.170
+DUT=192.168.3.2
 
 curl -X POST --data '{"baudrate":"5"}' http://$DUT/__/config/port/RS232/1 | jq
 
